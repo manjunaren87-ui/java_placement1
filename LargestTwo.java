637ef72ea5f82c7b8aa441c6cddb281a8f5ed6ea
naren <manjunaren87@gmail.com>
2026-08-26 11:05:20 +0530

    Largest of 2 Numbers

---
+++ LargestTwo.java
@@ -0,0 +1,10 @@
+public class LargestTwo {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if (a > b)
+            System.out.println(a + " is largest");
+        else
+            System.out.println(b + " is largest");
+    }
+}
