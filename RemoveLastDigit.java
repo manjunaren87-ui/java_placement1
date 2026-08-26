17fff24fa4d2ccd863cd0c912d667881eadba79c
naren <manjunaren87@gmail.com>
2026-08-26 12:04:54 +0530

    Remove Last Digit

---
+++ RemoveLastDigit.java
@@ -0,0 +1,9 @@
+public class RemoveLastDigit {
+    public static void main(String[] args) {
+        int n = 12345;
+
+        n = n / 10;
+
+        System.out.println("After removing last digit = " + n);
+    }
+}
