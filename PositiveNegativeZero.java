69a725973347062a5ad7b2df9bf208db23918317
naren <manjunaren87@gmail.com>
2026-08-26 11:04:04 +0530

    positive or negative or zero

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,12 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        int n = -5;
+
+        if (n > 0)
+            System.out.println("Positive");
+        else if (n < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
